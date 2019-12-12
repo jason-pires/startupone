dcdae95c051cc598f7c902f490b88a3985b38115
Jason Pires <jason.pires@outlook.com>
2019-12-11 21:58:54 -0300

    Legenda no grafico de outliers

--- mvp/assets/pages/google-chart-demo.js
+++ mvp/assets/pages/google-chart-demo.js
@@ -135,11 +135,11 @@ $(function () {
 
   function drawChart4() {
     var data = google.visualization.arrayToDataTable([
-      ['Mon', 20, 28, 38, 45],
-      ['Tue', 31, 38, 55, 66],
-      ['Wed', 50, 55, 77, 80],
-      ['Thu', 77, 77, 66, 50],
-      ['Fri', 68, 66, 22, 15]
+      ['MG', 20, 28, 38, 45],
+      ['SC', 31, 38, 55, 66],
+      ['RJ', 50, 55, 77, 80],
+      ['ES', 77, 77, 66, 50],
+      ['SP', 68, 66, 22, 15]
       // Treat first row as data as well.
     ], true);
 
